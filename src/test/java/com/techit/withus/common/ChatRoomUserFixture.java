f6059009637f72aa36427b56606ead99d8a7d1b6
GangminRYOU <dbrkdals1994@gmail.com>
2023-08-24 15:49:38 +0900

    test : chat 도메인 엔티티 관련 test 추가

---
+++ src/test/java/com/techit/withus/common/ChatRoomUserFixture.java
@@ -0,0 +1,21 @@
+package com.techit.withus.common;
+
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.techit.withus.web.chat.domain.ChatRoomUser;
+
+public class ChatRoomUserFixture {
+    private static final Long TEST_ID_A = 1L;
+
+    public static ChatRoomUser createDefaultChatRoomUser(){
+        return ChatRoomUser.builder()
+            .chatRoom(ChatRoomFixture.createDefaultChatRoomUser())
+            .build();
+    }
+
+    public static ChatRoomUser createChatRoomUserWithId(Long id){
+        ChatRoomUser chatRoomUser = createDefaultChatRoomUser();
+        ReflectionTestUtils.setField(chatRoomUser, "id", id);
+        return chatRoomUser;
+    }
+}
