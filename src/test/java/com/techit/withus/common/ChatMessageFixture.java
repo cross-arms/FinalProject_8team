8cbcef10596c072d8caa45dda9d74fca748db799
GangminRYOU <dbrkdals1994@gmail.com>
2023-08-24 22:25:57 +0900

    feat : Chat도메인 관련 리포지토리 테스트 용 상수 추가

--- src/test/java/com/techit/withus/common/ChatMessageFixture.java
+++ src/test/java/com/techit/withus/common/ChatMessageFixture.java
@@ -7,6 +7,8 @@ import com.techit.withus.web.chat.domain.ChatMessage;
 public class ChatMessageFixture {
     private static final String TEST_MESSAGE_A = "test message A";
     private static final Long TEST_ID_A = 1L;
+    public static final Integer TEST_PAGE_NUMBER = 0;
+    public static final Integer TEST_PAGING_SIZE = 3;
     public static ChatMessage createDefaultChatMessage(){
         return ChatMessage.builder()
             .message(TEST_MESSAGE_A)
--- src/test/java/com/techit/withus/common/ChatRoomFixture.java
+++ src/test/java/com/techit/withus/common/ChatRoomFixture.java
@@ -3,13 +3,19 @@ package com.techit.withus.common;
 import com.techit.withus.web.chat.domain.ChatRoom;
 
 public class ChatRoomFixture {
-    private static final Long TEST_ID_A = 1L;
+    public static final Long TEST_CHATROOM_ID_A = 1L;
     public static final String INVALID_LENGTH_ROOM_NAME = createRoomNameWithMoreThan20Texts();
     private static final String TEST_ROOM_NAME_A = "Room A001";
 
-    public static ChatRoom createDefaultChatRoomUser(){
+    public static ChatRoom createDefaultChatRoom(){
         return ChatRoom.builder()
             .roomName(TEST_ROOM_NAME_A)
+            .build();
+    }
+
+    public static ChatRoom createTestAChatRoom(){
+        return ChatRoom.builder()
+            .roomName("방 이름 1")
             .build();
     }
 
