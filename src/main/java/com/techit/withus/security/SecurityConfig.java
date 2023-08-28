1899d0b50b54ea1f2133b900cee0fb31a2c69a99
gyeongnam <gyeongnamdev@gmail.com>
2023-08-28 16:23:01 +0900

    feat: H2 Database로 변경하면서 나타나는 Security 에러 해결

--- src/main/java/com/techit/withus/security/SecurityConfig.java
+++ src/main/java/com/techit/withus/security/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.techit.withus.security;
 
 import com.techit.withus.jwt.JwtConfig;
+import jakarta.servlet.SessionTrackingMode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +14,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import java.util.stream.Stream;
 
 @Configuration
 @RequiredArgsConstructor
@@ -22,16 +29,16 @@ public class SecurityConfig
     @Bean
     public PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer()
-    {
-        return (web) -> web.ignoring().requestMatchers(
-                "v3/api-docs", "swagger-ui/**"
-        );
-    }
+    private static final String[] PERMIT_ALL_PATTERNS
+            = new String[]
+            {
+                    "v3/api-docs/**",
+                    "swagger-ui/**"
+            };
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   HandlerMappingIntrospector handlerMappingIntrospector) throws Exception
     {
         http
                 .csrf(AbstractHttpConfigurer::disable)
@@ -39,10 +46,16 @@ public class SecurityConfig
                 .addFilterBefore(jwtConfig, AuthorizationFilter.class)
                 .authorizeHttpRequests(
                         request -> request
-                                .requestMatchers("/**")
+                                .requestMatchers(PathRequest.toH2Console())
+                                .permitAll()
+                                .requestMatchers(
+                                        Stream
+                                                .of(PERMIT_ALL_PATTERNS)
+                                                .map(AntPathRequestMatcher::antMatcher)
+                                                .toArray(AntPathRequestMatcher[]::new))
                                 .permitAll()
                                 .anyRequest()
-                                .permitAll());
+                                .authenticated());
         return http.build();
     }
 }
