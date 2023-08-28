f41a9e5497306038f2be6e25758a743d202a80fc
gyeongnam <gyeongnamdev@gmail.com>
2023-08-28 22:57:52 +0900

    feat: SecurityFilterChain 오류 수정

--- src/main/java/com/techit/withus/security/SecurityConfig.java
+++ src/main/java/com/techit/withus/security/SecurityConfig.java
@@ -1,13 +1,11 @@
 package com.techit.withus.security;
 
 import com.techit.withus.jwt.JwtConfig;
-import jakarta.servlet.SessionTrackingMode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,10 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
-
-import java.util.stream.Stream;
 
 @Configuration
 @RequiredArgsConstructor
@@ -29,16 +24,9 @@ public class SecurityConfig
     @Bean
     public PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
 
-    private static final String[] PERMIT_ALL_PATTERNS
-            = new String[]
-            {
-                    "v3/api-docs/**",
-                    "swagger-ui/**"
-            };
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http,
-                                                   HandlerMappingIntrospector handlerMappingIntrospector) throws Exception
+                                                   HandlerMappingIntrospector introspector) throws Exception
     {
         http
                 .csrf(AbstractHttpConfigurer::disable)
@@ -49,10 +37,9 @@ public class SecurityConfig
                                 .requestMatchers(PathRequest.toH2Console())
                                 .permitAll()
                                 .requestMatchers(
-                                        Stream
-                                                .of(PERMIT_ALL_PATTERNS)
-                                                .map(AntPathRequestMatcher::antMatcher)
-                                                .toArray(AntPathRequestMatcher[]::new))
+                                        new MvcRequestMatcher(
+                                                introspector,
+                                                "/**"))
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated());
