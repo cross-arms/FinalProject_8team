48b7fe5496102de5d93beac6590905e2dfa8b2c8
gyeongnam <gyeongnamdev@gmail.com>
2023-08-28 23:24:09 +0900

    feat: Users(유저 엔티티)에서 더이상 username을 사용하지 않을 예정.email이 username의 역할을 대신하면서

--- src/main/java/com/techit/withus/web/users/domain/entity/Users.java
+++ src/main/java/com/techit/withus/web/users/domain/entity/Users.java
@@ -1,5 +1,6 @@
 package com.techit.withus.web.users.domain.entity;
 
+import com.techit.withus.oauth.OAuth2Provider;
 import com.techit.withus.web.users.domain.enumeration.Roles;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -22,9 +23,16 @@ public class Users
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
-    private String username;
+    private String email; // 이메일 형식 (OAuth2 사용자와 통일)으로 로그인
     private String password;
-    private String email;
+
+    private String username;
+    /**
+     * deprecated. email 형식의 아이디를 사용하므로 username 컬럼을 삭제할 예정임.
+     * username을 참조하는 곳에서 문제가 발생하기 때문에 놔둠.
+      */
+
+    private String nickname;
     private String phone;
 
     private Long point;
@@ -38,6 +46,8 @@ public class Users
     @UpdateTimestamp
     private Timestamp modifiedDate;
 
+    @Enumerated(EnumType.STRING)
+    private OAuth2Provider provider;
     @Enumerated(EnumType.STRING)
     private Roles role;
 }
