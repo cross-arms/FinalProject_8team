76bea2a25c2184173da56d2cadfb1567ba71330b
GangminRYOU <dbrkdals1994@gmail.com>
2023-08-30 14:56:03 +0900

    feat : 채팅방 유저 강퇴를 위한 repository 메소드 추가

--- src/main/java/com/techit/withus/web/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/techit/withus/web/chat/repository/ChatRoomRepository.java
@@ -1,6 +1,9 @@
 package com.techit.withus.web.chat.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.techit.withus.common.exception.EntityNotFoundException;
 import com.techit.withus.common.exception.ErrorCode;
@@ -10,5 +13,8 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     default ChatRoom findByIdOrThrow(Long id){
         return findById(id).orElseThrow(() -> new EntityNotFoundException(ErrorCode.CHATROOM_NOT_EXIST, id));
     }
+
+    @Query("select cr from ChatRoom cr join fetch cr.chatRoomUsers cru where cr.id=:roomId")
+    Optional<ChatRoom> findByIdJoinFetch(Long roomId);
 }
 
