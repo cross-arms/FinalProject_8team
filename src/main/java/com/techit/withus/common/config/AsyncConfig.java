e162e153c187a046ae607c2c69dac4bf2051500e
GangminRYOU <dbrkdals1994@gmail.com>
2023-09-06 14:49:01 +0900

    feat : 알림 비동기처리를 위한 AsyncConfig 설정 클래스 추가

---
+++ src/main/java/com/techit/withus/common/config/AsyncConfig.java
@@ -0,0 +1,23 @@
+package com.techit.withus.common.config;
+
+import java.util.concurrent.Executor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig implements AsyncConfigurer {
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(3);
+        taskExecutor.setMaxPoolSize(10);
+        taskExecutor.setQueueCapacity(50);
+        taskExecutor.setThreadNamePrefix("async-thread");
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+}
