05b15f2669b85907155d97cede700aef86896405
GangminRYOU <dbrkdals1994@gmail.com>
2023-09-06 15:04:16 +0900

    refactor: url 생성 로직 전략패턴에 추가

--- src/main/java/com/techit/withus/web/notification/domain/strategy/FollowNotificationStrategy.java
+++ src/main/java/com/techit/withus/web/notification/domain/strategy/FollowNotificationStrategy.java
@@ -2,10 +2,16 @@ package com.techit.withus.web.notification.domain.strategy;
 
 public class FollowNotificationStrategy implements NotificationStrategy{
 
-    private static final String FOLLOW_MESSAGE_FORMAT = "%s 님이 팔로우 당신을 팔로우 했습니다.";
+    private static final String FOLLOW_NOTIFICATION_FORMAT = "%s 님이 팔로우 당신을 팔로우 했습니다.";
+    private static final String FOLLOW_RELATED_URL = "http://localhost:8080/%s";
 
     @Override
     public String createMessage(String senderEmail) {
-        return String.format(FOLLOW_MESSAGE_FORMAT, senderEmail);
+        return String.format(FOLLOW_NOTIFICATION_FORMAT, senderEmail);
+    }
+
+    @Override
+    public String createRelatedUrl(String relatedUri) {
+        return String.format(FOLLOW_RELATED_URL, relatedUri);
     }
 }
