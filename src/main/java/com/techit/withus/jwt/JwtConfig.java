aa09e768b80e9202b0829ca5ae4942127d4ea3e3
gyeongnam <gyeongnamdev@gmail.com>
2023-09-10 00:56:38 +0900

    refactor: JWT 리팩토링 및 로그아웃 관련 로직 추가

--- src/main/java/com/techit/withus/jwt/JwtConfig.java
+++ src/main/java/com/techit/withus/jwt/JwtConfig.java
@@ -1,15 +1,18 @@
 package com.techit.withus.jwt;
 
+import com.techit.withus.common.exception.AuthenticationException;
+import com.techit.withus.common.exception.ErrorCode;
+import com.techit.withus.redis.service.RefreshTokenService;
 import com.techit.withus.security.SecurityService;
 import com.techit.withus.security.SecurityUser;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
@@ -17,10 +20,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Stream;
 
 @Slf4j
 @Configuration
@@ -28,13 +28,28 @@ import java.util.stream.Stream;
 public class JwtConfig extends OncePerRequestFilter
 {
     private final SecurityService securityService;
+    private final RefreshTokenService refreshTokenService;
     private final JwtService jwtService;
 
+    /**
+     * 1. 요청의 헤더를 분석한다.
+     *     -> Authorization 헤더인지, 맞다면 Bearer 스키마를 포함하는 어세스 토큰을 제시하는지
+     * 2. 어세스 토큰을 분석한다.
+     *     -> 어세스 토큰을 파싱하여 문제가 없는지 확인한다.
+     * 3. 리프레시 토큰을 분석한다.
+     *     -> 1) 어세스 토큰으로부터 유저의 정보를 얻는다.
+     *     -> 2) 유저의 정보를 키 값으로, 리프레시 토큰을 밸류 값으로 가지는 DB를 통해 리프레시 토큰을 얻는다.
+     *     -> 3) 해당 리프레시 토큰과 쿠키로부터 얻은 리프레시 토큰이 일치하는지 확인한다.
+     * 4. SpringSecurity가 다음 역할을 수행할 수 있도록 데이터를 넘겨준다.
+     *     -> 유저의 정보를 Authentication > SecurityContext > SecurityContextHolder 순서대로 담는다.
+     */
+
+    /*
     // Token 인증이 필요없는 URL을 따로 지정, 일단 모두 토큰이 필요없는 상태로 지정
     private static final List<String> EXCLUDE_URL
-            = List.of(
-                    "/**");
+            = List.of("auth/**", "resources/**");
 
+    // Token
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
         return EXCLUDE_URL
@@ -44,28 +59,56 @@ public class JwtConfig extends OncePerRequestFilter
                                 .equalsIgnoreCase(
                                         request.getServletPath()));
     }
+     */
+
+    private String getRefreshTokenInCookie(HttpServletRequest request)
+    {
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies != null)  {
+            for (Cookie cookie : cookies) {
+                if ("refreshToken".equals(cookie.getName()))
+                    return cookie.getValue();
+            }
+        }
+        return null;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException
     {
-        String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        // 1. 요청의 헤더를 분석한다.
+        String token = jwtService.getAccessToken(request);
 
-        if (header != null && header.startsWith("Bearer ")) {
-            String token = header.split(" ")[1];
+        // 2. 어세스 토큰을 분석한다.
+        if (token != null && jwtService.validateToken(token)) {
 
-            if (jwtService.isValid(token)) {
-                String username = jwtService.getUsername(token);
-                SecurityUser securityUser = securityService.loadUserByUsername(username);
+            // 3. 리프레시 토큰을 분석한다.
+            // 3-1) 어세스 토큰으로부터 얻은 유저의 정보
+            String subject = jwtService.getSubject(token);
+            // 3-2) 해당 유저의 정보로 DB에서 조회한 리프레시 토큰
+            String refreshTokenInRedis = refreshTokenService.findRefreshToken(subject);
+            // 3-3) 쿠키로부터 얻은 리프레시 토큰
+            String refreshTokenInCookie = this.getRefreshTokenInCookie(request);
 
-                SecurityContext context = SecurityContextHolder.createEmptyContext();
-                AbstractAuthenticationToken abstractAuthenticationToken
-                        = new UsernamePasswordAuthenticationToken(securityUser, token, securityUser.getAuthorities());
-                context.setAuthentication(abstractAuthenticationToken);
-                SecurityContextHolder.setContext(context);
+            if (refreshTokenInRedis.equals(refreshTokenInCookie)) {
+                this.setSecurityContextHolder(refreshTokenInRedis);
             }
         }
         filterChain.doFilter(request, response);
+    }
+
+    private void setSecurityContextHolder(String token)
+    {
+        String email = jwtService.getSubject(token);
+        SecurityUser securityUser = securityService.loadUserByUsername(email);
+
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        AbstractAuthenticationToken abstractAuthenticationToken
+                = new UsernamePasswordAuthenticationToken(securityUser, token, securityUser.getAuthorities());
+        context.setAuthentication(abstractAuthenticationToken);
+        SecurityContextHolder.setContext(context);
     }
 }
