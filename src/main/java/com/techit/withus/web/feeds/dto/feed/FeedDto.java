940a26c27c18a84e9c9b1a0897cc29961b0fe5c0
이재환 <rkdldk810@gmail.com>
2023-09-12 18:22:48 +0900

    resolve conflict

--- src/main/java/com/techit/withus/web/feeds/dto/feed/FeedDto.java
+++ src/main/java/com/techit/withus/web/feeds/dto/feed/FeedDto.java
@@ -1,5 +1,6 @@
 package com.techit.withus.web.feeds.dto.feed;
 
+<<<<<<<< Updated upstream:src/main/java/com/techit/withus/web/feeds/dto/feed/FeedDto.java
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.techit.withus.common.exception.InvalidValueException;
 import com.techit.withus.web.comments.dto.CommentDto.CommentResponse;
@@ -145,3 +146,26 @@ public class FeedDto {
         }
     }
 }
+========
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class FeedDTO {
+    private Long feedId;
+    private String writer;
+    private String scope;
+    private String title;
+    private String content;
+    // 좋아요 수, 댓글 수
+    private Long likeCount;
+    private Long commentCount;
+    // 이미지 URL
+    List<String> imageUrls;
+}
+>>>>>>>> Stashed changes:src/main/java/com/techit/withus/web/feeds/domain/dto/FeedDto.java
