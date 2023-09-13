6868f400551674e3c023ed87d7140d82b9fb8b76
gyeongnam <gyeongnamdev@gmail.com>
2023-09-13 22:27:08 +0900

    refactor: 이미지 저장위치 변경

--- src/main/java/com/techit/withus/security/CorsConfig.java
+++ src/main/java/com/techit/withus/security/CorsConfig.java
@@ -15,9 +15,10 @@ public class CorsConfig
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
+        config.addExposedHeader("Authorization");
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
