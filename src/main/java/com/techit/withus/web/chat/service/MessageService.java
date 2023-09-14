738c0959bdba658d5a05d1e9b7876839d50064c4
GangminRYOU <dbrkdals1994@gmail.com>
2023-09-14 11:35:03 +0900

    refactor : 이벤트 트랜잭션 분리로 인한 receiver id 전달로 수정

--- src/main/java/com/techit/withus/web/chat/service/MessageService.java
+++ src/main/java/com/techit/withus/web/chat/service/MessageService.java
@@ -1,7 +1,6 @@
 package com.techit.withus.web.chat.service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Pageable;
@@ -16,7 +15,6 @@ import com.techit.withus.web.chat.domain.ChatMessage;
 import com.techit.withus.web.chat.domain.ChatRoomUser;
 import com.techit.withus.web.chat.repository.ChatMessageRepository;
 import com.techit.withus.web.chat.repository.ChatRoomUserRepository;
-import com.techit.withus.web.users.domain.entity.Users;
 
 import lombok.RequiredArgsConstructor;
 
@@ -37,11 +35,11 @@ public class MessageService {
             .orElseThrow(() -> new EntityNotFoundException(ErrorCode.INVALID_ROOM_MEMBER));
         ChatMessage chatMessage = request.toChatMessage();
         chatMessage.associateChatRoomUser(foundChatRoomUser);
-        List<Users> receivers = chatRoomUsers.stream()
-            .map(ChatRoomUser::getUser)
-            .filter(user -> !username.equals(user.getEmail()))
-            .collect(Collectors.toList());
-        chatMessage.publishMessageEvent(publisher, receivers);
+        List<Long> receiversIds = chatRoomUsers.stream()
+            .filter(user -> !username.equals(user.getUser().getEmail()))
+            .map(user -> user.getUser().getUserId())
+            .toList();
+        chatMessage.publishMessageEvent(publisher, receiversIds);
         return chatMessageRepository.save(chatMessage).getId();
     }
 
