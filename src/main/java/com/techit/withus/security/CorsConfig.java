dde7000c26db881badf79d42ae61b38265a465bf
GangminRYOU <dbrkdals1994@gmail.com>
2023-09-14 10:46:46 +0900

    refactor : STOMP 채팅관련 cors설정 변경

--- src/main/java/com/techit/withus/security/CorsConfig.java
+++ src/main/java/com/techit/withus/security/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
