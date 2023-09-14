cf0bbd324c5da995a31d073e037e9853fee0f048
이재환 <rkdldk810@gmail.com>
2023-09-14 20:15:05 +0900

    좋아요 기능 수정

--- src/main/java/com/techit/withus/web/feeds/controller/LikeController.java
+++ src/main/java/com/techit/withus/web/feeds/controller/LikeController.java
@@ -1,10 +1,12 @@
 package com.techit.withus.web.feeds.controller;
 
 import com.techit.withus.common.dto.ResultDTO;
+import com.techit.withus.security.SecurityUser;
 import com.techit.withus.web.feeds.domain.dto.LikeDto.RegisterLikeRequest;
 import com.techit.withus.web.feeds.service.LikeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +20,11 @@ public class LikeController {
 
     @PutMapping("/api/v1/like")
     public ResultDTO saveLike(
-        @RequestBody RegisterLikeRequest request
+        @RequestBody RegisterLikeRequest request,
+        @AuthenticationPrincipal SecurityUser user
     ) {
+        request.setUserId(user.getUserId());
+
         likeService.saveLike(request);
 
         return ResultDTO.builder()
@@ -29,8 +34,11 @@ public class LikeController {
 
     @PutMapping("/api/v1/like/cancel")
     public ResultDTO cancelLike(
-        @RequestBody RegisterLikeRequest request
+        @RequestBody RegisterLikeRequest request,
+        @AuthenticationPrincipal SecurityUser user
     ) {
+        request.setUserId(user.getUserId());
+
         likeService.cancelLike(request);
 
         return ResultDTO.builder()
